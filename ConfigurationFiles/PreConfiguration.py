2fb7c8fffd49255729e41aca75f092397fd58e10
Felix Bonjour <felix@embedded-bonjour.ch>
2024-02-11 09:02:28 +0100

    style: PEP8

--- ConfigurationFiles/PreConfiguration.py
+++ ConfigurationFiles/PreConfiguration.py
@@ -1,26 +1,26 @@
-#config for Standby
+# config for Standby
 StandbyActive = False
 
-#config for NR1-UI-Remote
+# config for NR1-UI-Remote
 NR1UIRemoteActive = False
 
-#config for LED
+# config for LED
 ledActive = False
 ledTechnology = None
 
-#config for GPIO Buttons
+# config for GPIO Buttons
 oledRtrLeft = 13
-oledRtrRight = 5 
+oledRtrRight = 5
 oledRtrBtn = 6
 
 
-#config for Display:
+# config for Display:
 DisplayTechnology = "spi1322"
 NowPlayingLayout = "Spectrum-Center"
 oledrotation = 2
 
-#config for timers:
+# config for timers:
 oledPause2StopTime = 5.0
 
-#config for spectrum
+# config for spectrum
 SpectrumActive = True
